f625f9a1ffab292b8bf4a33cafbf271e2be20251
dkbeom <namebdk@gmail.com>
2022-09-08 17:55:26 +0900

    printBlock, eraseBlock 분리 & virtual 블럭 추가

--- js/main.js
+++ js/main.js
@@ -27,7 +27,6 @@ function init() {
     for (let i = 0; i < ROWS; i++) {
         prependNewLine();
     }
-
     createNewBlock();
 }
 
@@ -41,33 +40,112 @@ function prependNewLine() {
     table.prepend(row);
 }
 
-// 블럭 렌더링
-function renderBlock(move="", moveType="") {
-    const {type, direction, X, Y} = movingBlock;
+// 새로운 블럭 생성
+function createNewBlock() {
+
+    clearInterval(fallInterval);
+    fallInterval = setInterval(()=>{
+        moveBlock('Y', 1);
+    },duration);
+
+    const blockTypeList = Object.keys(BLOCKS);
+    const random = Math.floor(Math.random() * blockTypeList.length);
+
+    // 생성되는 새 블럭
+    movingBlock.type = blockTypeList[random];
+    movingBlock.direction = 0;
+    movingBlock.X = 3;
+    movingBlock.Y = 0;
+
+    // 새로 생성되는 블럭이 바로 테스트 불통하면 게임 종료
+    if(testBlock(movingBlock)){
+        gameover();
+    }
+    
+    renderMovingBlock();
+    renderVirtualBlock();
+}
+
+// 무빙 블럭 렌더링
+function renderMovingBlock(move="", moveType="") {
+
     // 기존 무빙 제거
-    const moving = document.querySelectorAll(".moving");
-    moving.forEach(movingElement=>{
-        movingElement.classList.remove(type, "moving");
-    })
-    // 새 무빙 추가
-    BLOCKS[type][direction].some(element=>{
+    eraseBlock("moving");
+
+    if(move === false && moveType === 'Y'){
+        seizeBlock();
+        checkMatch();
+        createNewBlock();
+    } else {
+        // 새 무빙 추가
+        printBlock(movingBlock, "moving");
+    }
+}
+
+// 가상 블럭 렌더링
+function renderVirtualBlock() {
+
+    const virtualBlock = Object.assign({}, movingBlock);
+
+    // 장애물(블럭, 바닥)을 만날 때까지 밑으로 내린다
+    while(true){
+        virtualBlock.Y++;
+        // 테스트 불통하면
+        if(testBlock(virtualBlock)){
+            virtualBlock.Y--;
+            break;
+        }
+    }
+
+    // 기존 가상 블럭 지우기
+    eraseBlock("virtual");
+
+    // 가상 블럭 프린팅
+    printBlock(virtualBlock, "virtual");
+}
+
+// 블럭 프린팅
+function printBlock({type, direction, X, Y}, virtualOrMoving) {
+    BLOCKS[type][direction].forEach(element=>{
         const x = element[0] + X;
         const y = element[1] + Y;
-        // 블럭이 밑으로 내려오다 땅이나 블럭에 부딛혔을 때, seized 상태로 변환
-        if(move === false && moveType === 'Y'){
-            seizeBlock();
-            checkMatch();
-            createNewBlock();
-            return true;
-        } else {
+        // 가상 블럭 프린팅
+        if(virtualOrMoving === "virtual"){
+            table.children[y].children[x].classList.add("virtual");
+        }
+        // 무빙 블럭 프린팅
+        else if(virtualOrMoving === "moving") {
             table.children[y].children[x].classList.add(type, "moving");
+        }
+        else {
+
         }
     })
 }
 
+// 블럭 프린팅 지우기
+function eraseBlock(virtualOrMoving) {
+    // 가상 블럭 지우기
+    if(virtualOrMoving === "virtual") {
+        const virtual = document.querySelectorAll(".virtual");
+        virtual.forEach(virtualElement=>{
+            virtualElement.classList.remove("virtual");
+        })
+    }
+    // 무빙 블럭 지우기
+    else if(virtualOrMoving === "moving") {
+        const moving = document.querySelectorAll(".moving");
+        moving.forEach(movingElement=>{
+            movingElement.classList.remove(movingBlock.type, "moving");
+        })
+    }
+    else {
+
+    }
+}
+
 // 블럭이 불가능한지 테스트(불가능하면 true 반환)
-function testBlock() {
-    const {type, direction, X, Y} = movingBlock;
+function testBlock({type, direction, X, Y}) {
     return BLOCKS[type][direction].some(element=>{
         const x = element[0] + X;
         const y = element[1] + Y;
@@ -84,11 +162,11 @@ function moveBlock(moveType, amount) {
     movingBlock[moveType] += amount;
     
     // 테스트 불통하면
-    if(testBlock()){
+    if(testBlock(movingBlock)){
         movingBlock[moveType] -= amount;
         move = false;
     }
-    renderBlock(move, moveType);
+    renderMovingBlock(move, moveType);
 }
 
 // 블럭을 돌리는 함수
@@ -96,10 +174,10 @@ function changeDirection() {
     movingBlock["direction"] === 3 ? movingBlock["direction"] = 0 : movingBlock["direction"] += 1;
     
     // 테스트 불통하면
-    if(testBlock()){
+    if(testBlock(movingBlock)){
         movingBlock["direction"] === 0 ? movingBlock["direction"] = 3 : movingBlock["direction"] -= 1;
     }
-    renderBlock();
+    renderMovingBlock();
 }
 
 // 블럭을 바로 떨어뜨리는 함수
@@ -139,31 +217,6 @@ function checkMatch() {
     })
 }
 
-// 새로운 블럭 생성
-function createNewBlock() {
-
-    clearInterval(fallInterval);
-    fallInterval = setInterval(()=>{
-        moveBlock('Y', 1);
-    },duration);
-
-    const blockTypeList = Object.keys(BLOCKS);
-    const random = Math.floor(Math.random() * blockTypeList.length);
-
-    // 생성되는 새 블럭
-    movingBlock.type = blockTypeList[random];
-    movingBlock.direction = 0;
-    movingBlock.X = 3;
-    movingBlock.Y = 0;
-
-    // 새로 생성되는 블럭이 바로 테스트 불통하면 게임 종료
-    if(testBlock()){
-        gameover();
-    }
-
-    renderBlock(false, null);
-}
-
 // 게임 오버
 function gameover() {
     console.log("게임오버");
@@ -177,18 +230,22 @@ document.addEventListener('keydown', e => {
         // 오른쪽 화살표
         case 39:
             moveBlock('X', 1);
+            renderVirtualBlock();
             break;
         // 왼쪽 화살표
         case 37:
             moveBlock('X', -1);
+            renderVirtualBlock();
             break;
         // 아래쪽 화살표
         case 40:
             moveBlock('Y', 1);
+            renderVirtualBlock();
             break;
         // 위쪽 화살표
         case 38:
             changeDirection();
+            renderVirtualBlock();
             break;
         // 스페이스바
         case 32:
